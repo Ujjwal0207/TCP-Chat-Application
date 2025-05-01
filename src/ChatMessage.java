fe5027f3d5113eb42329a03a0d0e230384830e4f
Ujjwal0207 <ujjwalgoyal.ajmercity@gmail.com>
2025-05-01 12:16:59 +0530

    Nineteen Commit: Declare and Initialize Variables, Create Constructor for ChatMessage Class.

--- src/ChatMessage.java
+++ src/ChatMessage.java
@@ -1,7 +1,19 @@
+import java.io.Serializable;
+
 /**
  * @author Adrian Adewunmi
  * @date 25 Sept 2022
  * @description ChatMessage Class
  */
-public class ChatMessage {
+public class ChatMessage implements Serializable {
+
+    static final int WHOISIN = 0, MESSAGE = 1, LOGOUT = 2;
+    private int type;
+    private String message;
+
+    public ChatMessage(int type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
 }
